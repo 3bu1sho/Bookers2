63af9b04c3bac177de431b533d14b36eca96180a
3bu1sho <3bu1sho@gmail.com>
2020-11-26 02:04:29 +0000

    [Finish] 4-10

--- Bookers2/app/models/user.rb
+++ Bookers2/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     
     validates :name, presence: true
     validates :name, uniqueness: true
-    validates :name,    length: { maximum: 20 }
-    validates :name,    length: { minimum: 2 } 
-    validates :introduction,    length: { maximum: 50 }
+    validates :name, length: { maximum: 20 }
+    validates :name, length: { minimum: 2 } 
+    validates :introduction, length: { maximum: 50 }
 end
